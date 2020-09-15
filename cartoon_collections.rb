a0ad57823e617f3bd5e8e901f57944c6bcd3bb17
Devin Cooper <dcoope12@asu.edu>
2020-09-15 02:57:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,12 +12,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-   calls.each do |call|
-     if call.length > 4
-    return true
-  else
-    return false
-
+   calls.any? do |call|
+     call.length > 4
 end
 
 def find_the_cheese# code an argument here
